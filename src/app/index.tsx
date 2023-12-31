76a37e3aa87c024da01ec041a5748682b953229a
novaardiansyah <novaardiansyah78@gmail.com>
2023-12-31 23:51:30 +0700

    Installing react router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native'
-import DayListItem from './src/components/core/DayListItem'
+import DayListItem from '../components/core/DayListItem'
 
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter'
 import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc'
@@ -11,7 +11,7 @@ import { useEffect } from 'react'
 SplashScreen.preventAutoHideAsync()
 const days = [...Array(24)].map((val, index) => index + 1)
 
-export default function App() {
+export default function HomeScreen() {
   const [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
     Amatic: AmaticSC_400Regular,
