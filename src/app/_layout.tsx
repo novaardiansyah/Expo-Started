06febcc23868a837f6b3066e460087efc3fe1e18
novaardiansyah <novaardiansyah78@gmail.com>
2024-01-01 21:33:55 +0700

    Load font and splash screen in the root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter'
+import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc'
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
+
+import * as SplashScreen from 'expo-splash-screen'
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold
+  })
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+  
+  if (!fontsLoaded && !fontError) return <ActivityIndicator />
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Nova Ardiansyah' }} />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native'
-import { Link } from 'expo-router'
-
+import { StyleSheet, View, FlatList } from 'react-native'
 import DayListItem from '../components/core/DayListItem'
 
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter'
-import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc'
-
-import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
-
-SplashScreen.preventAutoHideAsync()
 const days = [...Array(24)].map((val, index) => index + 1)
 
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold
-  })
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync()
-    }
-  }, [fontsLoaded, fontError])
-  
-  if (!fontsLoaded && !fontError) return <ActivityIndicator />
-
   return (
     <View style={styles.container}>
       <FlatList  
